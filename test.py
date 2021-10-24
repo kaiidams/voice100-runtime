6d6c740e1960f62df2c6bf85fc1649dbd6b2bc2e
Katsuya Iida <katsuya.iida@gmail.com>
2021-10-24 15:07:48 +0900

    update

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ TEST_TEXT = \
 
 class Voice100RuntimeTest(unittest.TestCase):
     def test_stt(self):
-        stt = voice100_runtime.from_pretrained('stt_en')
+        stt = voice100_runtime.load('stt_en')
         waveform, sample_rate = sf.read('output.wav')
         text = stt(waveform, sample_rate)
         print(text)
 
     def test_tts(self):
-        tts = voice100_runtime.from_pretrained('tts_en')
+        tts = voice100_runtime.load('tts_en')
         waveform, sample_rate = tts(TEST_TEXT)
         sf.write('output.wav', waveform, sample_rate, 'PCM_16')
 
