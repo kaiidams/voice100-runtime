94b3058240c76d4fa506411fd4e440f66bbb4aeb
Katsuya Iida <katsuya.iida@gmail.com>
2021-12-11 12:40:38 +0900

    update

--- voice100_runtime/__init__.py
+++ voice100_runtime/__init__.py
@@ -7,15 +7,20 @@ CACHE_DIR = os.path.expanduser("~/.cache/voice100_runtime")
 
 MODEL_URLS = {
     "stt_en": [
-        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.1/stt_en_conv_base_ctc-20210619.onnx"
+        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.2/stt_en_conv_base_ctc-20211125.onnx"
     ],
     "tts_en": [
         "https://github.com/kaiidams/voice100-runtime/releases/download/v0.1/ttsalign_en_conv_base-20210808.onnx",
-        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.1/ttsaudio_en_conv_base-20210811.onnx",
+        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.1/ttsaudio_en_conv_base-20210811.onnx"
+    ],
+    "stt_ja": [
+        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.2/stt_ja_conv_base_ctc-20211127.onnx"
+    ],
+    "tts_ja": [
+        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.2/ttsalign_ja_conv_base-20211118.onnx",
+        "https://github.com/kaiidams/voice100-runtime/releases/download/v0.2/ttsaudio_ja_conv_base-20211118.onnx"
     ],
 }
-
-# Downloading: "https://github.com/pytorch/vision/archive/master.zip" to /home/kaiida/.cache/torch/hub/master.zip
 
 
 def download_model(url: str) -> str:
@@ -32,12 +37,12 @@ def download_model(url: str) -> str:
 
 
 def load(name):
-    if name == "stt_en":
+    if name == "stt_en" or name == "stt_ja":
         model_path = download_model(MODEL_URLS[name][0])
         from .stt import STT
 
         return STT(model_path)
-    elif name == "tts_en":
+    elif name == "tts_en" or name == "tts_ja":
         align_model_path = download_model(MODEL_URLS[name][0])
         audio_model_path = download_model(MODEL_URLS[name][1])
         from .tts import TTS
